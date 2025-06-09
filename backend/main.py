cf9ff32c048de5919f33bd1d75930f1619cd6913
Masterson <gabriel.masterson2602@gmail.com>
2025-06-08 21:06:48 -0300

    a

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello, World!</h1>"
 
+@app.route("/sum/<int:a>/<int:b>")
+def sum(a,b):
+    return f"{a} + {b} = {a + b}"
 
 if __name__ == "__main__":
     app.run(debug=True)
